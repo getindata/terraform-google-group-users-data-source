aaf716c5189ea0a4d5dfedc6f69f51ab15852b70
Maciej PIetrzykowski <maciej.pietrzykowski@getindata.com>
2023-04-03 10:53:22 +0200

    fix constraints

--- versions.tf
+++ versions.tf
@@ -4,9 +4,9 @@ terraform {
   required_version = ">= 0.15.0"
 
   required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "3.1.1"
+    google = {
+      source  = "hashicorp/google"
+      version = "4.59.0"
     }
   }
 }
