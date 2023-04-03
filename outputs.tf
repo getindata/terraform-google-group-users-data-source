0abcc8a892085debf905db8b5abe3bc313be778a
Maciej PIetrzykowski <maciej.pietrzykowski@getindata.com>
2023-04-03 12:01:31 +0200

    change outpu to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "users" {
   value       = [for u in data.google_cloud_identity_group_memberships.members.memberships : u.preferred_member_key[0].id]
-  description = "List of Users from Google"
+  description = "List of Users from Google group"
 }
