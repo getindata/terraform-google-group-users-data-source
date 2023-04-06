9c3f75d4985fe463899105ab7df672efed2922cd
Maciej PIetrzykowski <maciej.pietrzykowski@getindata.com>
2023-04-06 11:11:41 +0200

    fix

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,5 +1,5 @@
 module "terraform_google_group_users_data_source" {
-  source       = "../../"
+  source       =  "../../"
   group_email  =  "example-group@google.com"
-  parent       = "customers/{customer_id}"
+  parent       =  "customers/{customer_id}"
 }
