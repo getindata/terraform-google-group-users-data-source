aef31b9fdb506426660eca9a34cc1e6e6f70ef12
Piotr Mossakowski <piotr.mossakowski@getindata.com>
2023-04-07 12:25:39 +0200

    docs: update variable description

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "group_email" {
-  description = "Group ID"
+  description = "(Required) The Group email under which to lookup the Membership names."
   type        = string
 }
 variable "parent" {
-  description = "Parent group ID"
+  description = "(Required) The resource name of the entity under which this Group resides in the Cloud Identity resource hierarchy. Must be of the form identitysources/{identity_source_id} for external-identity-mapped groups or customers/{customer_id} for Google Groups."
   type        = string
 }
