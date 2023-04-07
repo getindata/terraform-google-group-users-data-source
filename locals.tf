89cbe93db7fa5b683d6d04d09bea5a90ac22428a
Maciej PIetrzykowski <maciej.pietrzykowski@getindata.com>
2023-04-07 09:06:49 +0200

    add null

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  groups_map      = { for g in data.google_cloud_identity_groups.groups.groups : g.group_key[0].id => g.name }
-  search_in_a_map = lookup(local.groups_map, var.group_email)
+  groups_map = { for g in data.google_cloud_identity_groups.groups.groups : g.group_key[0].id => g.name }
+  group_id   = lookup(local.groups_map, var.group_email, null)
 }
--- main.tf
+++ main.tf
@@ -2,5 +2,9 @@ data "google_cloud_identity_groups" "groups" {
   parent = var.parent
 }
 data "google_cloud_identity_group_memberships" "members" {
-  group = local.search_in_a_map
+  group = local.group_id
+}
+provider "google" {
+  user_project_override = true
+  project               = "gid-seed-project"
 }
