41193d45d947c9727df5852a60fe55f96b7d04e0
MaciejMacQ <55737733+maciejmacq-dev@users.noreply.github.com>
2023-04-07 12:33:42 +0200

    feat: Translation group email to group id (#8)
    * new changes
    
    * terraform-docs: automated action
    
    * add default to var
    
    * fix
    
    * fix
    
    * terraform-docs: automated action
    
    * nb
    
    * fmt
    
    * fix
    
    * fixs
    
    * revert main.tf
    
    * fix
    
    * add null
    
    * terraform-docs: automated action
    
    * fixs
    
    * fixs
    
    * docs: update variable description
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Piotr Mossakowski <piotr.mossakowski@getindata.com>

--- locals.tf
+++ locals.tf
@@ -1,2 +1,4 @@
 locals {
+  groups_map = { for g in data.google_cloud_identity_groups.groups.groups : g.group_key[0].id => g.name }
+  group_id   = lookup(local.groups_map, var.group_email, null)
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,8 @@
-# Example, compulsory input variable
-variable "group" {
-  description = "Group ID"
+variable "group_email" {
+  description = "(Required) The Group email under which to lookup the Membership names."
   type        = string
 }
 variable "parent" {
-  description = "Parent group ID"
+  description = "(Required) The resource name of the entity under which this Group resides in the Cloud Identity resource hierarchy. Must be of the form identitysources/{identity_source_id} for external-identity-mapped groups or customers/{customer_id} for Google Groups."
   type        = string
 }
